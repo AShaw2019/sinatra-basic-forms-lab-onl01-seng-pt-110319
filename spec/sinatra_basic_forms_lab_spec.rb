50a037859897fd5b9be9a17d9abb7767e5392fc5
Ashley Twomey <aprier091012@gmail.com>
2020-03-01 17:17:40 +0000

    Automatically backed up by Learn

--- spec/sinatra_basic_forms_lab_spec.rb
+++ spec/sinatra_basic_forms_lab_spec.rb
@@ -1,46 +1,87 @@
 describe App do
 
-  describe 'GET /' do
-    
+  describe 'GET /newteam' do
     it 'sends a 200 status code' do
-      get '/'
+      get '/newteam'
       expect(last_response.status).to eq(200)
     end
 
-    it 'renders welcome' do 
-      visit '/'
-      expect(page).to have_link("Click Here To List A Puppy")
-    end
-  end
-
-  describe 'GET /NEW' do 
-    it 'sends a 200 status code' do
-      get '/new'
-      expect(last_response.status).to eq(200)
-    end
-
-    it 'renders the form' do
-      visit '/new'
+    it 'renders basketball team form' do 
+      visit '/newteam'
       expect(page).to have_selector("form")
       expect(page).to have_field(:name)
-      expect(page).to have_field(:breed)
-      expect(page).to have_field(:age)
+      expect(page).to have_field(:coach)
+      expect(page).to have_field(:pg)
+      expect(page).to have_field(:sg)
+      expect(page).to have_field(:sf)
+      expect(page).to have_field(:pf)
+      expect(page).to have_field(:c)
     end
   end
 
-  describe 'POST /' do
-    it "displays the puppy" do 
-      visit '/new'
+  describe 'POST /team' do
+    it "displays the basketball team name in the browser" do 
+      visit '/newteam'
 
-      fill_in(:name, :with => "Butch")
-      fill_in(:breed, :with => "Mastiff")
-      fill_in(:age, :with => "6 months")
+      fill_in(:name, :with => "Bballers")
       click_button "submit"
-      expect(page).to have_text("Puppy Name:\nButch")
-      expect(page).to have_text("Puppy Breed:\nMastiff")
-      expect(page).to have_text("Puppy Age:\n6 months")
+      expect(page).to have_text("Team Name: Bballers")
     end
-  end
 
-  
+    it "displays the coach's name in the browser" do
+      visit '/newteam'
+
+      fill_in(:coach, :with => "Walter")
+      click_button "submit"
+
+      expect(page).to have_text("Coach: Walter")
+    end
+
+    it "displays the point guard's name in the browser" do
+      visit '/newteam'
+
+      fill_in(:pg, :with => "Jeff")
+      click_button "submit"
+
+      expect(page).to have_text("Point Guard: Jeff")
+    end
+
+    it "displays the shooting guard's name in the browser" do
+      visit '/newteam'
+
+      fill_in(:sg, :with => "Ralph")
+      click_button "submit"
+
+      expect(page).to have_text("Shooting Guard: Ralph")
+    end
+
+    it "displays the power forward's name in the browser" do
+      visit '/newteam'
+
+      fill_in(:pf, :with => "Danny")
+      click_button "submit"
+
+      expect(page).to have_text("Power Forward: Danny")
+    end
+
+    it "displays the shooting gaurd's name in the browser" do
+      visit '/newteam'
+
+      fill_in(:sg, :with => "Joe")
+      click_button "submit"
+
+      expect(page).to have_text("Shooting Guard: Joe")
+    end
+
+    it "displays the center's name in the browser" do
+      visit '/newteam'
+
+      fill_in(:c, :with => "Avi")
+      click_button "submit"
+
+      expect(page).to have_text("Center: Avi")
+    end
+
+
+  end
 end
